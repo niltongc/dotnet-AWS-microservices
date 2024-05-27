95ea7b6368ed96414ae1e528525d256b45633ad9
nilton <ngcquimica@gmail.com>
2024-05-27 16:49:10 -0300

    course completed

---
+++ Microservices-Kafka/Microservices-RabbitMQ/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        const string url = "amqps://admin:Senha@1231212_b-2aa373e5-773f-4ea3-b6fb-b164128316d8.mq.us-east-1.amazonaws.com:5671";
+        const string queueName = "my-test-queue";
+
+
+        var model = new {Name = "John Doe"};
+        var serializedModel = JsonConvert.SerializeObject(model);
+        var bytes = Encoding.UTF8.GetBytes(serializedModel);
+
+
+        Console.Clear();
+
+        var connectionFactory = new RabbitMQ.Client.ConnectionFactory()
+        {
+            Uri = new Uri(url)
+        };
+
+        var connection = connectionFactory.CreateConnection();
+        var channel = connection.CreateModel();
+        channel.QueueDeclare(queueName, false, true, false, null);
+        channel.BasicPublish("",queueName, false, null, bytes);
+
+        var consumer = new RabbitMQ.Client.Events.EventingBasicConsumer(channel);
+
+        consumer.Received += (sender, e) =>
+        {
+            var stringMessage = Encoding.UTF8.GetString(e.Body.ToArray());
+            Console.WriteLine(stringMessage);
+        };
+
+        channel.BasicConsume(queueName, true, "", true, true, null, consumer);
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Microservices-Kafka/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Text.Json.Serialization;
+using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+
+
+public class Program
+{
+    public class UserCreated
+    {
+        public string Name { get; set; }
+    }
+
+    public static void Main(string[] args)
+    {
+        const string server = "127.0.0.1:9092";
+        const string topicName = "kafka-test";
+
+        var adminClient = new AdminClientBuilder(
+            new AdminClientConfig
+            {
+                BootstrapServers = server,
+                SecurityProtocol = SecurityProtocol.Plaintext
+            }
+        ).Build();
+
+        var metaData = adminClient.GetMetadata(TimeSpan.FromMinutes(1));
+
+        if (metaData.Topics.All(x => x.Topic != topicName))
+        {
+            adminClient.CreateTopicsAsync(new[]
+            {
+                new TopicSpecification
+                {
+                    Name = topicName,
+                    NumPartitions = 1,
+                    ReplicationFactor = 1
+                }
+            }).Wait();
+        }
+
+        // var producer = new ProducerBuilder<int, String>(new ProducerConfig
+        // {
+        //     BootstrapServers = server,
+        //     SecurityProtocol = SecurityProtocol.Plaintext
+        // })
+        // .Build();
+
+        // var myEvent = new UserCreated
+        // {
+        //     Name = "John Doe"
+        // };
+
+        // var serializeObject = JsonConvert.SerializeObject(myEvent);
+
+        // producer.Produce(topicName, new Message<int, string>
+        // {
+        //     Key = 100,
+        //     Value = serializeObject
+        // });
+
+
+        var consumer = new ConsumerBuilder<int, String>(new ConsumerConfig
+        {
+            BootstrapServers = server,
+            SecurityProtocol = SecurityProtocol.Plaintext,
+            GroupId = "Group"
+        }).Build();
+
+        consumer.Subscribe(topicName);
+        var result = consumer.Consume(TimeSpan.FromSeconds(30));
+        
+    }
+}
+
+
+
+
